da0c5b8e538206404c2f212cc0d69cf28089e073
Alex <acramanalex@gmail.com>
2022-08-25 18:41:13 +0100

    wn change

--- rss/settings/base.py
+++ rss/settings/base.py
@@ -118,6 +118,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
