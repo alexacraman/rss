ba90c15a38cad797e2fe78fa2f0c75de7aa5120e
Alex <acramanalex@gmail.com>
2024-01-06 18:52:07 +0000

    urgh

--- rss/settings/pro.py
+++ rss/settings/pro.py
@@ -37,7 +37,7 @@ SECURE_SSL_REDIRECT             = False
 
 # Use dj_database_url to parse the DATABASE_URL environment variable
 # This variable should be set in your environment or in a .env file
-DATABASE_URL = env.get('DATABASE_URL')
+DATABASE_URL = env('DATABASE_URL')
 
 # Update the DATABASES setting in your Django project settings
 DATABASES = {
