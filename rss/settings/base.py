0328aba6a6b7eacf839a10d503f6a57e2df0f101
Alex <acramanalex@gmail.com>
2024-01-09 13:03:46 +0000

    alter db settings

--- rss/settings/base.py
+++ rss/settings/base.py
@@ -98,18 +98,18 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'rss.wsgi.application'
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-import dj_database_url
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-DATABASES['default']['CONN_MAX_AGE'] = 500
+# import dj_database_url
+
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
+# DATABASES['default']['CONN_MAX_AGE'] = 500
 
 AUTH_PASSWORD_VALIDATORS = [
     {
